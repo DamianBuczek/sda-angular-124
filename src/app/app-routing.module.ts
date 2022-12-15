3822a03d99b9074dd69184695691c16d29e613b6
Szymon Roszyk <roszyk.szymon@gmail.com>
2022-12-15 21:03:36 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoperComponent } from './components/stoper/stoper.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "stoper", component: StoperComponent},
+  {path: "usersList", component: UsersListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
